861428a551603113eb914b7d6c0a2ef282138599
shamanmk6 <102096024+shamanmk6@users.noreply.github.com>
2024-05-20 23:45:39 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,13 @@
-
+const dotenv=require ('dotenv')
+dotenv.cconfig();
 import { initializeApp } from "firebase/app";
 
-
+const FIREBASE_KEY = process.env.FIREBASE_KEY
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBHRU2ClF1CVNNgEcY6sXA-oB8IP02B_QI",
-    authDomain: "fir-e9d16.firebaseapp.com",
-    projectId: "fir-e9d16",
-    storageBucket: "fir-e9d16.appspot.com",
-    messagingSenderId: "8693088751",
-    appId: "1:8693088751:web:17b67d82c59a8f43cc761a",
-    measurementId: "G-GPG91NY1GB"
+    FIREBASE_KEY
   };
 // Initialize Firebase
 const Firebase = initializeApp(firebaseConfig);
